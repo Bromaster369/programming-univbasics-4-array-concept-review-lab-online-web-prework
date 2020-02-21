a5a04d0d078825e2a57f0bc62f3e98c80c3ad56a
Christian Pardo <chrispardopersonal@gmail.com>
2020-02-21 22:23:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,12 +11,12 @@ end
 
 def find_max_value(array)
   x = 0
-  array.length.times do |index|
+  array.length.times { |index|
   if array[index] > x
     x = array [index]
   end
+  }
   x
-  
 end
 
 def find_min_value(array)
